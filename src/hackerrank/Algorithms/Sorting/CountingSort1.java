490498f48efd7985032fa671fefcf6d9cff79708
AR Roman <ar_roman@yahoo.com>
2017-03-30 00:03:12 -0400

    .

---
+++ src/hackerrank/Algorithms/Sorting/CountingSort1.java
@@ -0,0 +1,25 @@
+package hackerrank.Algorithms.Sorting;
+
+import java.util.Scanner;
+
+public class CountingSort1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[100];
+		
+		for(int i=0;i<n;i++){
+			int input=sc.nextInt();
+			arr[input]++;
+		}
+		sc.close();
+		
+		for(int i=0;i<n;i++){
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
